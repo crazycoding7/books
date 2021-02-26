a81dc1a0e6d231208bc9c62fbf548d5160d1e92e
qishuai <644172593@qq.com>
2021-02-26 10:32:08 +0800

    add 面试题菜单

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,13 +10,13 @@ module.exports = {
     sidebar: [
       {
         title: 'Android基础',   // 必要的
-        path: '/Android/01基础机制/',      // 可选的, 应该是一个绝对路径
+        path: '/Android/',      // 可选的, 应该是一个绝对路径
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1,    // 可选的, 默认值是 1
         children: [
             '/Android/01基础机制/1.Android系统启动流程',
-            '/Android/01基础机制/2.Binder进程通信机制.md'
-            // '/Android/超越100知识总结.md'
+            '/Android/01基础机制/2.Binder进程通信机制.md',
+            '/Android/07面试题/超越100知识总结.md'
         ]
       },
        {
